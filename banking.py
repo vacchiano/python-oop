2d85e95cc08832a39a2749a5dbf7f172c2747119
Dom Vacchiano <djvacchiano@gmail.com>
2020-06-07 21:28:01 -0500

    added the ability to depost, withdraw, and check balance

--- banking.py
+++ banking.py
@@ -10,11 +10,43 @@ class User:
         self.balance += amount
         print(self.name, "Deposited $"+str(amount))
     def withdraw(self, amount):
+        if self.balance - amount < 0:
+            print("ERROR; Not Enough Funds: you have", "$"+str(self.balance), "remaining")
+            return
+            #print('WARNING:', self.name, "your balance is", "$"+str(self.balance))
         self.balance -= amount
         print(self.name, "Withdrew $"+str(amount))
+    def getBalance(self):
+        print(self.name, "you have $" + str(self.balance), "in your account")
 
 dom = User("Dom Vacchiano", "domo@dom.com")
-print ("$"+str(dom.balance))
+jack = User("Jack Doe", "jack@mail.com")
+tina = User("Tina Fry", "tina@msn.com")
+
+spaces = "-"*10
+
+dom.getBalance()
+jack.getBalance()
+tina.getBalance()
+print(spaces)
+
 dom.deposit(300)
-print("$"+str(dom.balance))
+dom.deposit(500)
+dom.deposit(100)
+dom.withdraw(100)
+dom.getBalance()
+print(spaces)
+
+jack.deposit(500)
+jack.deposit(150)
+jack.withdraw(200)
+jack.withdraw(150)
+jack.getBalance()
+print(spaces)
+tina.deposit(600)
+tina.withdraw(100)
+tina.withdraw(50)
+tina.withdraw(75)
+tina.withdraw(500)
+tina.getBalance()
 
